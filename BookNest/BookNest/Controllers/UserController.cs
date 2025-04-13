0e583589cf1fd013c091e3cb94a68a3c58b2ddbf
Dev Raj Khadka <devrajkhadka941@gmail.com>
2025-04-13 20:24:00 +0545

    Feat: User CRUD

--- BookNest/BookNest/Controllers/UserController.cs
+++ BookNest/BookNest/Controllers/UserController.cs
@@ -4,13 +4,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookNest.Controllers
 {
-    [Route("api/controller")]
     [ApiController]
-    public class UserController: ControllerBase
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
     {
         private readonly AppDbContext dbContext;
 
-        public UserController (AppDbContext dbContext)
+        public UserController(AppDbContext dbContext)
         {
             this.dbContext = dbContext;
         }
@@ -22,5 +22,41 @@ namespace BookNest.Controllers
             dbContext.SaveChanges();
             return Ok(user);
         }
+
+
+        [HttpGet]
+        public IActionResult GetAllUser()
+        {
+            List<User> usersList = dbContext.Users.ToList();
+            return Ok(usersList);
+        }
+
+
+        [HttpGet("/{id}")]
+        public IActionResult GetUserById(Guid id)
+        {
+            User? user = dbContext.Users.FirstOrDefault(st => st.Id == id);
+            return user != null ? Ok(user) : NotFound("User Not Found");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(User std, Guid id)
+        {
+            var user = dbContext.Users.FirstOrDefault(st => st.Id == id);
+            if (user is not null)
+            {
+                user.Email = std.Email;
+                user.PhoneNumber = std.PhoneNumber;
+                user.Password = std.Password;
+                user.FullName = std.FullName;
+                user.UserName = std.UserName;
+                user.Role = std.Role;
+
+                dbContext.SaveChanges();
+                return Ok(user);
+            }
+            return NotFound("Khai id nai veteyena");
+        }
+
     }
 }
--- BookNest/BookNest/Data/Entities/User.cs
+++ BookNest/BookNest/Data/Entities/User.cs
@@ -1,9 +1,31 @@
-﻿namespace BookNest.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookNest.Data.Entities
 {
     public class User
     {
+        [Key]
         public Guid Id { get; set; }
-        public string? Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? UserName { get; set; }
+
+        [StringLength(100)]
+        public string? FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Phone]
         public string? PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string? Role { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
     }
 }
