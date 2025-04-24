3c56a0c3d418cbadad6bc8636c49a81770fca0e9
Arpit Gupta <gupta.14arpit@gmail.com>
2025-04-24 21:55:33 +0545

    Feat: Author Controller Added

---
+++ BookNest/BookNest/Controllers/AuthorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookNest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+    }
+}
