f965c18709c81a66c3d26b73ffb00fd69da537a3
Arpit Gupta <gupta.14arpit@gmail.com>
2025-05-09 17:14:58 +0545

    Update: Order Entity updated to store order received

--- BookNest/BookNest/Data/Entities/Order.cs
+++ BookNest/BookNest/Data/Entities/Order.cs
@@ -7,6 +7,7 @@ namespace BookNest.Data.Entities
         public Guid Id { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public long UserId { get; set; }
+        public bool OrderReceived { get; set; } = false;
         public List<OrderItem> Items { get; set;} = new List<OrderItem>();
         public User? User { get; set; }
     }
